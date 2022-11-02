91f8a36fc50672917b3dcd9bcfc9c18d1f8d6d67
Matt Oake <matt@Matts-MacBook-Air.local>
2022-11-02 14:53:39 -0300

    First commit.

---
+++ script.js
@@ -0,0 +1,6 @@
+// Time to try and build a web version of your Tic Tac Toe game
+// You will want to have 9 boxes that a user can click on
+// - Depending on the user turn the clicked box should change to X or O
+// - A User should only be able to select one box per turn
+// - A User should not be able to select a box that is already selected
+// - When game is over (whether a win or tie) the users should be prompted to play again and the game should be reset
