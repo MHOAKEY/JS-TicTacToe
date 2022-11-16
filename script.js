ce0d6bd1e991d0f3d123d8394b606a38b3d11d74
Matt Oake <matt@Matts-MacBook-Air.local>
2022-11-16 16:05:17 -0400

    Removed unused code and repositioned text.innerHTML = player x turn.

--- script.js
+++ script.js
@@ -10,24 +10,15 @@ let playerTurn = true;
 let text = document.getElementById("text");
 const xGamePiece = "X";
 const oGamePiece = "O";
-const one = document.getElementById("1");
-const two = document.getElementById("2");
-const three = document.getElementById("3");
-const four = document.getElementById("4");
-const five = document.getElementById("5");
-const six = document.getElementById("6");
-const seven = document.getElementById("7");
-const eight = document.getElementById("8");
-const nine = document.getElementById("9");
 const gameSpaces = document.querySelectorAll(".gameSpace");
 
 gameSpaces.forEach(addEventListenerToGameSpaces);
 
-text.innerHTML += "Player X turn";
-
 function addEventListenerToGameSpaces(gameSpace) {
   gameSpace.addEventListener("click", useGamePiece);
 }
+
+text.innerHTML += "Player X turn";
 
 function checkWin() {
   if (
